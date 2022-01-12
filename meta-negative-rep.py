4db857c2e903b23bc598b62a6b80bc3ca502bf54
eungkim <eb.kim@snu.ac.kr>
2022-01-12 20:55:48 +0900

    test

--- meta-negative-rep.py
+++ meta-negative-rep.py
@@ -31,8 +31,7 @@ args = parser.parse_args()
 
 # torch settings
 torch.manual_seed(816)
-# os.environ["CUDA_VISIBLE_DEVICES"] = '0, 1, 2, 3'
-os.environ["CUDA_VISIBLE_DEVICES"] = '0'
+os.environ["CUDA_VISIBLE_DEVICES"] = '0, 1, 2, 3'
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"device: {device}")
 
