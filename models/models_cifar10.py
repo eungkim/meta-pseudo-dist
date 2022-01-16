e979e47161f4d64dd14382831307ff2852813c50
eungkim <eb.kim@snu.ac.kr>
2022-01-16 23:31:25 +0900

    test code

--- models/models_cifar10.py
+++ models/models_cifar10.py
@@ -92,8 +92,8 @@ class ResNet(nn.Module):
         x = self.layer1(x)
         x = self.layer2(x)
         x = self.layer3(x)
-        x = F.avg_pool2d(x, x.size()[3])
         print(x.size())
+        x = F.avg_pool2d(x, x.size()[3])
         x = x.view(x.size(0), -1)
 
         x = F.relu(self.fc1_bn(self.fc1(x)))
